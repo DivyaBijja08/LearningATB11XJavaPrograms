16b25df9760873f7e1b2c4140f9f0190719ff55b
DivyaBijja08 <divyabunny8@gmail.com>
2025-03-11 23:54:25 +0530

    Prcatice Comment

---
+++ src/Lab01_Comment.java
@@ -0,0 +1,18 @@
+public class Lab01_Comment {
+    public static void main(String[] args) {
+        //single line comment - CTRL+/ - to comment and uncomment
+        System.out.println("Hi 1 !!!");
+        System.out.println("Hi 2 !!!");
+        System.out.println("Hi 3 !!!");
+//        System.out.println("Hi !!!");
+//        System.out.println("Hi !!!");
+//        System.out.println("Hi !!!");
+     // Multiline comment - CTRL+SHIFT+/ - to comment and uncomment
+/*
+        System.out.println("Hi Multi line");
+        System.out.println("Hi Multi line");*/
+        System.out.println("Hi Multi line");
+        System.out.println("Hi Multi line");
+
+    }
+}
